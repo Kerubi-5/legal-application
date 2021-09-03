93e16e73c7a04eecccf1b54f7a96a99429779505
John Kim Querobines <querobinesj@gmail.com>
2021-09-03 16:05:20 +0800

    added lawyer

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,7 @@
         <li class="nav-item"><a data-bs-toggle="tab" href="#lawyer" class="nav-link">Lawyer</a></li>
     </ul>
     <div class="tab-content">
+        <!-- User -->
         <div class="tab-pane active" id="client">
             <form method="POST" action="{{ route('register') }}">
                 @csrf
@@ -44,9 +45,48 @@
                 <button type="submit" class="btn btn-primary">Register</button>
             </form>
         </div>
+        <!-- Lawyer -->
         <div class="tab-pane" id="lawyer">
             <form action="POST">
-                Lawyer upcomming
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Username</label>
+                    <input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                        aria-describedby="name">
+                    @error('name')
+                    <span class="invalid-feedback is-invalid" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input name="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                        id="email" aria-describedby="email">
+                    @error('email')
+                    <span class="invalid-feedback is-invalid" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input name="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                        id="password">
+                    @error('password')
+                    <span class="invalid-feedback is-invalid" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                    <input name="password_confirmation" type="password" class="form-control" id="password_confirmation">
+                </div>
+                <div class="mb-3">
+                    <label for="formFileMultiple" class="form-label">Input the following files for verification</label>
+                    <input class="form-control" type="file" id="formFileMultiple" multiple>
+                </div>
+
+                <input name="individual_type" type="text" value="lawyer" hidden>
+
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
+
             </form>
         </div>
     </div>
